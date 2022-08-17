ac0177fb1f0e71b6d1bd210426911e2672660d65
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-08-17 17:07:58 +0700

    make new class and apply to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-//import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <p>lorem ipsum, atau ringkasanya lorem ipsum bla bla bla</p>
+      <Header />
+      <h1>Rendering Elements</h1>
     </div>
   );
 }
